fd584449d584e0db7bb4f27ade65987eff4a001b
Aldokler <xdavidpastor@gmail.com>
2023-08-30 10:12:55 -0600

    prueba

---
+++ mergeSort.c
@@ -0,0 +1,15 @@
+#include "sync.h"
+
+void merge(){
+
+}
+
+void mergeSort(int arr[]){
+    int n = sizeof(arr) / sizeof(arr[0]);
+    if (n > 1){
+        for (int i = 0; i < n/2; i++){
+            
+        }
+        mergeSort()
+    }
+}
