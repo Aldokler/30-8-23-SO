13bf6b6fadb874a2f89fcc2b5c50446049efdf94
Aldokler <xdavidpastor@gmail.com>
2023-08-30 10:57:32 -0600

    CD

---
+++ sync.c
@@ -0,0 +1,6 @@
+#include <math.h>
+#include <time.h>  
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+
---
+++ sync.h
@@ -0,0 +1,7 @@
+#ifndef _SYNC
+#define _SYNC
+
+
+
+#include  "sync.c"
+#endif
