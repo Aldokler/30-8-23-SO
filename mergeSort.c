1ac8eb9d0bc0c9d6c2427476b552b4b50df42894
Aldokler <xdavidpastor@gmail.com>
2023-09-05 20:07:01 -0600

    f

--- mergeSort.c
+++ mergeSort.c
@@ -18,7 +18,6 @@ struct mergeParam { int p, q, r; } ;
 
 // Merge two subarrays L and M into arr
 void *merge(void *arg){
-    pthread_mutex_lock(&lock);
     struct mergeParam * params = arg;
     int p, q, r;
     p = params->p;
@@ -28,48 +27,67 @@ void *merge(void *arg){
     // Create L ← A[p..q] and M ← A[q+1..r]
     int n1 = q - p + 1;
     int n2 = r - q;
-
-    int L[n1], M[n2];
-    pthread_mutex_unlock(&lock);
-
-    for (int i = 0; i < n1; i++)
-        L[i] = arr[p + i];
-    for (int j = 0; j < n2; j++)
-        M[j] = arr[q + 1 + j];
-
     // Maintain current index of sub-arrays and main array
     int i, j, k;
     i = 0;
     j = 0;
     k = p;
 
-    // Until we reach either end of either L or M, pick larger among
-    // elements L and M and place them in the correct position at A[p..r]
-    while (i < n1 && j < n2){
-        if (L[i] <= M[j]){
-            arr[k] = L[i];
-            i++;
-        }
-        else{
+    if (n1 <= 0){
+        int M[n2];
+        for (int j = 0; j < n2; j++)
+            M[j] = arr[q + 1 + j];
+        while (j < n2){
             arr[k] = M[j];
             j++;
+            k++;
         }
-        k++;
+    } else if (n2 <= 0){
+        int L[n1];
+        for (int i = 0; i < n1; i++)
+            L[i] = arr[p + i];
+        while (i < n1){
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+    } else {
+        int L[n1], M[n2];
+
+        for (int i = 0; i < n1; i++)
+            L[i] = arr[p + i];
+        for (int j = 0; j < n2; j++)
+            M[j] = arr[q + 1 + j];
+
+        // Until we reach either end of either L or M, pick larger among
+        // elements L and M and place them in the correct position at A[p..r]
+        while (i < n1 && j < n2){
+            if (L[i] <= M[j]){
+                arr[k] = L[i];
+                i++;
+            }
+            else{
+                arr[k] = M[j];
+                j++;
+            }
+            k++;
+        }
+
+        // When we run out of elements in either L or M,
+        // pick up the remaining elements and put in A[p..r]
+        while (i < n1){
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2){
+            arr[k] = M[j];
+            j++;
+            k++;
+        }
     }
 
-    // When we run out of elements in either L or M,
-    // pick up the remaining elements and put in A[p..r]
-    while (i < n1){
-        arr[k] = L[i];
-        i++;
-        k++;
-    }
-
-    while (j < n2){
-        arr[k] = M[j];
-        j++;
-        k++;
-    }
     barrier(100);
 }
 
